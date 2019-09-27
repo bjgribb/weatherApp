cec04bd468384f391ddaa73a2128d7cc8d3e85f7
bjgribb <bjgribb@gmail.com>
2019-09-26 20:29:33 -0400

    details component

--- Minimus/src/app/app.module.ts
+++ Minimus/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
 import { AddCardComponent } from './ui/add-card/add-card.component';
+import { DetailsComponent } from './pages/details/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     WeatherCardComponent,
-    AddCardComponent
+    AddCardComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
--- Minimus/src/app/pages/home/home.component.ts
+++ Minimus/src/app/pages/home/home.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  city: string;
+  state: string;
+  temp: number;
+  hum: number;
+  wind: number;
 
-  constructor() { }
+  today: string;
 
-  ngOnInit() {
-  }
+  day1Name: string;
+  day1State: string;
+  day1Temp: number;
 
+  day2Name: string;
+  day2State: string;
+  day2Temp: number;
+
+  day3Name: string;
+  day3State: string;
+  day3Temp: number;
+
+  day4Name: string;
+  day4State: string;
+  day4Temp: number;
+
+  day5Name: string;
+  day5State: string;
+  day5Temp: number;
+
+  sub1: Subscription;
+  sub2: Subscription;
+  sub3: Subscription;
+  sub4: Subscription;
+  sub5: Subscription;
+
+  constructor(
+    public activeRouter: ActivatedRoute,
+    public weather: WeatherService
+  ) {}
+
+  ngOnInit() {}
 }
