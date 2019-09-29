f937d4ea49468854551c006aa78fab5b390db9ea
bjgribb <bjgribb@gmail.com>
2019-09-29 19:37:51 -0400

    refactoring

--- Minimus/src/app/ui/weather-card/weather-card.component.ts
+++ Minimus/src/app/ui/weather-card/weather-card.component.ts
@@ -1,15 +1,102 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, OnDestroy } from "@angular/core";
+import { first } from "rxjs/operators";
+import { WeatherService } from "../../services/weather.service";
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
-  selector: 'app-weather-card',
-  templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.css']
+  selector: "app-weather-card",
+  templateUrl: "./weather-card.component.html",
+  styleUrls: ["./weather-card.component.css"]
 })
-export class WeatherCardComponent implements OnInit {
+export class WeatherCardComponent implements OnInit, OnDestroy {
+  @Input() set city(city: string) {
+    this.cityName = city;
+    this.weather
+      .getWeather(city)
+      .pipe(first())
+      .subscribe(
+        payload => {
+          this.state = payload.weather[0].main;
+          this.temp = Math.ceil(payload.main.temp);
+        },
+        err => {
+          this.errorMessage = err.error.message;
+          setTimeout(() => {
+            this.errorMessage = "";
+          }, 3000);
+        }
+      );
 
-  constructor() { }
+    this.weather
+      .getForecast(city)
+      .pipe(first())
+      .subscribe(
+        payload => {
+          this.maxTemp = Math.round(payload[0].main.temp);
+          this.minTemp = Math.round(payload[0].main.temp);
+          for (const res of payload) {
+            if (
+              new Date().toLocaleDateString()("en-GB") ===
+              new Date(res.dt_txt).toLocaleDateString("en-GB")
+            ) {
+              this.maxTemp =
+                res.main.temp > this.maxTemp
+                  ? Math.round(res.main.temp)
+                  : this.maxTemp;
+              this.minTemp =
+                res.main.temp < this.minTemp
+                  ? Math.round(res.main.temp)
+                  : this.minTemp;
+            }
+          }
+        },
+        err => {
+          setTimeout(() => {
+            this.errorMessage = "";
+          }, 3000);
+        }
+      );
+  }
+
+  @Input() addMode;
+  @Output() cityStored = new EventEmitter();
+  citiesWeather: Object;
+  darkMode: boolean;
+  sub1: Subscription;
+  state: string;
+  temp: number;
+  maxTemp: number;
+  minTemp: number;
+  errorMessage: string;
+  cityName;
+  cityAdded = false;
+
+  constructor(
+    public weather: WeatherService,
+    public router: Router,
+    public ui: UiService,
+  ) {}
 
   ngOnInit() {
+    this.sub1 = this.ui.darkModeState.subscribe((isDark) => {
+      this.darkMode = isDark;
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub1.unsubscribe();
+  }
+
+  openDetails() {
+    if (!this.addMode) {
+      this.router.navigateByUrl('/details/' + this.cityName);
+    }
+  }
+
+  addCity() {
+    
   }
 
 }
