6e54167bccf82bf70f0c10a592385e232d33b409
bjgribb <bjgribb@gmail.com>
2020-02-29 09:50:17 -0500

    route home and small styling updates

--- Minimus/src/app/app-routing.module.ts
+++ Minimus/src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./pages/home/home.component";
-import { DetailsComponent } from "./pages/details/details/details.component";
-import { AddCardComponent } from './ui/add-card/add-card.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "details/:city", component: DetailsComponent },
-  { path: "add", component: AddCardComponent}
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
