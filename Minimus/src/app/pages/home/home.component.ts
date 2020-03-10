c1cf71f5eb5706ea6d6edf8bb1a65831dc339d6c
bjgribb <bjgribb@gmail.com>
2020-03-09 20:10:04 -0400

    rework of home component, weather by date then hour

--- Minimus/src/app/pages/home/home.component.ts
+++ Minimus/src/app/pages/home/home.component.ts
@@ -1,24 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-import { WeatherService } from 'src/app/services/weather.service';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import * as moment from 'moment';
 import { Observable } from 'rxjs';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  currentForecastData: any;
+export class HomeComponent {
+  currentForecastData: any[];
+  cityName: string;
   currentWeather$: Observable<any>;
-  constructor(public weather: WeatherService) {}
-
+  todayWeather: any;
+  weatherByDay: any;
   city = new FormControl('');
 
-  ngOnInit() {}
+  constructor(public weather: WeatherService) {}
 
   searchCity() {
     this.currentWeather$ = this.weather.getForecast(this.city.value);
-    this.currentWeather$.subscribe(res => console.log(res));
+
+    this.currentWeather$.subscribe(data => {
+      this.cityName = data.city.name;
+      this.createWeatherByDate(data.list);
+    });
+  }
+
+  createWeatherByDate(currentWeatherData) {
+    this.weatherByDay = [
+      {
+        date: moment(),
+        data: currentWeatherData.filter(x => new Date(x.dt * 1000).getDate() === moment().date())
+      },
+      {
+        date: moment().add(1, 'day'),
+        data: currentWeatherData.filter(
+          x =>
+            new Date(x.dt * 1000).getDate() ===
+            moment()
+              .add(1, 'day')
+              .date()
+        )
+      },
+      {
+        date: moment().add(2, 'day'),
+        data: currentWeatherData.filter(
+          x =>
+            new Date(x.dt * 1000).getDate() ===
+            moment()
+              .add(2, 'day')
+              .date()
+        )
+      },
+      {
+        date: moment().add(3, 'day'),
+        data: currentWeatherData.filter(
+          x =>
+            new Date(x.dt * 1000).getDate() ===
+            moment()
+              .add(3, 'day')
+              .date()
+        )
+      },
+      {
+        date: moment().add(4, 'day'),
+        data: currentWeatherData.filter(
+          x =>
+            new Date(x.dt * 1000).getDate() ===
+            moment()
+              .add(4, 'day')
+              .date()
+        )
+      }
+    ];
   }
 }
