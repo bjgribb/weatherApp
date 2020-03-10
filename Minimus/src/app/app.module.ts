da87478742f15c87438b831bfdab0d1dab259a76
bjgribb <bjgribb@gmail.com>
2020-03-09 20:09:34 -0400

    style update and imports

--- Minimus/src/app/app.module.ts
+++ Minimus/src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule, MatInputModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
-import { AddCardComponent } from './ui/add-card/add-card.component';
 import { DetailsComponent } from './pages/details/details/details.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule, MatCardModule } from '@angular/material';
+import { HomeComponent } from './pages/home/home.component';
 import { DegreesPipe } from './pipes/degrees.pipe';
+import { AddCardComponent } from './ui/add-card/add-card.component';
+import { WeatherCardComponent } from './ui/weather-card/weather-card.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, WeatherCardComponent, AddCardComponent, DetailsComponent, DegreesPipe],
@@ -26,7 +27,9 @@ import { DegreesPipe } from './pipes/degrees.pipe';
     MatFormFieldModule,
     ReactiveFormsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    NgxChartsModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
